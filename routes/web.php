690cec36a2e25d080724af76cf83f376a0254a9f
Lucio Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2024-03-14 12:41:04 -0400

    Rutas para el CRUD de books

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+$router->group(['prefix' => 'v1', 'middleware' => 'checkAuthorization'], function () use ($router) {
+    $router->get('/books', ['uses' => 'BookController@index']);
+    $router->get('/books/{id}', ['uses' => 'BookController@read']);
+    $router->post('/books', ['uses' => 'BookController@create']);
+    $router->put('/books/{id}', ['uses' => 'BookController@update']);
+    $router->patch('/books/{id}', ['uses' => 'BookController@patch']);
+    $router->delete('/books/{id}', ['uses' => 'BookController@delete']);
 });
